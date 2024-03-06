358ac3c68b0120777f56f644f48dd56075dda53d
rgmeseguer <rafael.garcia-meseguer@uv.es>
2024-03-06 17:46:04 +0100

    removed trash file

--- __init__-KhaosR-Desktop.py
+++
@@ -1,7 +0,0 @@
-#from MutResid.checkFiles import check_pdb_file, check_param_files
-#from MutResid.mutres import mutate_residue
-#from MutResid.genAmber import generate_amber_files
-
-#import MutResid.checkFiles
-#import MutResid.mutres
-#import MutResid.genAmber
